772cccc6ff5d6ca8b16cd88b6bb6bb1763dc292b
Bob Matsuoka <bob@matsuoka.com>
2026-02-26 09:54:55 -0500

    Add gitflow-analytics v3.13.15 formula

--- Formula/gitflow-analytics.rb
+++ Formula/gitflow-analytics.rb
@@ -3,27 +3,18 @@ class GitflowAnalytics < Formula
 
   desc "Analyze Git repositories for developer productivity insights"
   homepage "https://github.com/bobmatnyc/gitflow-analytics"
-  url "https://files.pythonhosted.org/packages/96/07/6a0082efa72731d212b5ba57da241f8f0caa4a577cb706ecff18e2ef61ec/gitflow_analytics-3.13.14.tar.gz"
-  sha256 "6f7ac0fedf3f761849ada6fdc87b92a45f902ebfc6bececab25ac0c253823b9a"
+  url "https://files.pythonhosted.org/packages/source/g/gitflow-analytics/gitflow_analytics-3.13.15.tar.gz"
+  sha256 "2f70462902314bb200141db53fe153fc5e2955cca0e3e42150fb40b4c70e36cc"
   license "MIT"
 
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   def install
-    # Create an isolated virtualenv
-    system Formula["python@3.11"].opt_bin/"python3.11", "-m", "venv", libexec
-
-    # Install the package and all its dependencies
-    system libexec/"bin/pip", "install", "-v", buildpath
-
-    # Create symlinks for the binaries
-    bin.install_symlink libexec/"bin/gitflow-analytics"
-    bin.install_symlink libexec/"bin/gfa"
+    virtualenv_install_with_resources
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/gitflow-analytics --version")
-    assert_match "Usage:", shell_output("#{bin}/gitflow-analytics --help")
     assert_match version.to_s, shell_output("#{bin}/gfa --version")
+    assert_match "Usage:", shell_output("#{bin}/gfa --help")
   end
 end
