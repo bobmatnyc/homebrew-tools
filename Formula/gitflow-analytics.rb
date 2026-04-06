04476d5f1dc15cd2c6db89f22976bce3d86d12f2
Bob Matsuoka <bob@matsuoka.com>
2026-04-06 13:43:38 -0400

    chore: update gitflow-analytics to v3.13.23

--- Formula/gitflow-analytics.rb
+++ Formula/gitflow-analytics.rb
@@ -3,8 +3,8 @@ class GitflowAnalytics < Formula
 
   desc "Analyze Git repositories for developer productivity insights"
   homepage "https://github.com/bobmatnyc/gitflow-analytics"
-  url "https://files.pythonhosted.org/packages/source/g/gitflow-analytics/gitflow_analytics-3.13.22.tar.gz"
-  sha256 "6f0afa37273c2259694b07ed652457663afb15b08bd723294392e388bd643807"
+  url "https://files.pythonhosted.org/packages/source/g/gitflow-analytics/gitflow_analytics-3.13.23.tar.gz"
+  sha256 "9d55e60062ffe337fe697be50f623a4955ecc7159ff5340179da8a7424350553"
   license "MIT"
 
   depends_on "python@3.12"
